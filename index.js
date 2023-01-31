a0cf3c152c5b1fbcc1d032d9776baba36271dfe9
Abby <abby.e.smith.85@gmail.com>
2023-01-31 10:21:15 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+let activity=`roller-skate`
+function saturdayFun(activity=`roller-skate`) {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(work=`go to the office`){
+    return (`This Monday, I will ${work}.`)
+}
+
+// function wrapAdjective(adjective = `special`){
+//     return (`You are ${adjective}!`)
+// }
+
+// let wrapAdjective = funtion(style = "*") {
+//     return function (adjective = "special"){
+//         return `You are ${style}${adjective}${style}!`
+//     }
+// }
+
+let wrapAdjective = function (style = `*`) {
+    return function(adjective = `special`){
+        return `You are ${style}${adjective}${style}!`
+    }
+}
